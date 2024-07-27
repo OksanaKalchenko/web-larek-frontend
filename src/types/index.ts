a8b83604358ac7bc3da5ebc328263f0c9c3f2995
Oksana Kalchenko <oksanakalchenko123@gmail.com>
2024-07-27 16:59:21 -0700

    документация на первое ревью

---
+++ src/types/index.ts
@@ -0,0 +1,62 @@
+// Интерфейс данных товара
+export interface IProduct {
+	id: string; // Уникальный идентификатор товара
+	description: string; // Описание товара
+	image: string; // URL изображения товара
+	title: string; // Наименование товара
+	category: TCategoryType; // Категория товара
+	price: number | null; //Цена товара
+}
+
+// Интерфейс товара в списке корзины
+export interface IProductInBasket extends IProduct {
+    index: number; // Индекс продукта в корзине
+  }
+
+export interface IContactForm {
+    email: string; // Электронная почта пользователя
+    phone: string; // Номер телефона пользователя
+}
+
+export interface IOrderForm {
+    payment: TPayMethod; // Способ оплаты
+    address: string; // Адрес доставки
+}
+
+
+export interface IOrder extends IOrderForm, IContactForm {
+	total: number; // Общая сумма заказа
+	items: string[]; // Массив товаров, включенных в заказ
+    checkValidation(data: Record<keyof TOrderFormData, string>): boolean;
+}
+
+// Интерфейс, описывающий глобальное состояние приложения
+export interface IAppState {
+    catalog: IProduct[];  // Массив продуктов в каталоге
+    basket: string[];   // Массив продуктов в корзине
+    preview: string | null;
+    order: IOrder | null;
+
+    setCatalog(items: IProduct[]): void;
+    addProduct(product: IProduct): void; // Добавляет продукт в корзину
+    removeProduct(product: IProduct): void; // Удаляет продукт из корзины
+}
+
+// Интерфейс успешной покупки
+export interface IOrderResult {
+    id: string;
+    total: number;
+}
+
+export type TPayMethod = 'cash' | 'card';
+
+export type TCategoryType = 'другое' | 'софт-скил' | 'дополнительное' | 'кнопка'| 'хард-скил';
+
+export type TProductMainPage = Pick<IProduct, 'category' | 'id' | 'image' | 'price' | 'title'>;
+
+export type TProductBasket = Pick<IProduct, 'id' | 'price' | 'title'>;
+
+export type TOrderFormData = Pick<IOrder, 'payment' | 'address' | 'email' | 'phone'>
+
+
+
